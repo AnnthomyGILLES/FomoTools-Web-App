297bd39f11023beae57ad9016133502c90f89a9f
AnnthomyGILLES <annthomy.gilles@gmail.com>
2022-09-08 17:44:51 +0200

    Black

--- apps/coinmarketcap/coinmarketcap_api.py
+++ apps/coinmarketcap/coinmarketcap_api.py
@@ -10,17 +10,21 @@ from loguru import logger
 from requests import Timeout, TooManyRedirects
 
 logger.remove()
-logger.add(sys.stderr, format="{time} | <lvl>{level}</lvl> {level.icon} | <lvl>{message}</lvl>", colorize=True)
+logger.add(
+    sys.stderr,
+    format="{time} | <lvl>{level}</lvl> {level.icon} | <lvl>{message}</lvl>",
+    colorize=True,
+)
 
 
 class CryptoMarket(object):
     _session = None
-    __DEFAULT_BASE_URL = 'https://pro-api.coinmarketcap.com'
+    __DEFAULT_BASE_URL = "https://pro-api.coinmarketcap.com"
     __DEFAULT_TIMEOUT = 30
 
     HEADERS = {
-        'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': '74868bae-9a4c-4b7f-9869-3a69f0b808ff'
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": "74868bae-9a4c-4b7f-9869-3a69f0b808ff",
     }
 
     ENDPOINTS = {
@@ -31,48 +35,52 @@ class CryptoMarket(object):
 
     def __init__(self, base_url=__DEFAULT_BASE_URL):
         self.base_url = base_url
-        cache_filename = 'coinmarketcap_cache'
+        cache_filename = "coinmarketcap_cache"
         self.cache_file_path = Path(tempfile.gettempdir(), cache_filename)
-        self.session = requests_cache.CachedSession(cache_name=str(self.cache_file_path), backend='sqlite',
-                                                    expire_after=864000)
+        self.session = requests_cache.CachedSession(
+            cache_name=str(self.cache_file_path), backend="sqlite", expire_after=864000
+        )
         self.session.headers.update(self.HEADERS)
 
     def get(self, url, **kwargs):
         url = self.base_url + url
         try:
-            response = self.session.get(url, params=kwargs, timeout=self.__DEFAULT_TIMEOUT)
+            response = self.session.get(
+                url, params=kwargs, timeout=self.__DEFAULT_TIMEOUT
+            )
 
             info = json.loads(response.text)
             return info
         except (ConnectionError, Timeout, TooManyRedirects) as e:
-            logger.warning(f"Error while fetching Url: {url}, with Parameters: {kwargs} and Error: {e}")
+            logger.warning(
+                f"Error while fetching Url: {url}, with Parameters: {kwargs} and Error: {e}"
+            )
 
     def get_quotes(self, **params: Union[dict, object]) -> pd.DataFrame:
         """See also: https://coinmarketcap.com/api/documentation/v1/#operation/getV2CryptocurrencyQuotesLatest"""
         url = self.ENDPOINTS["quotes"]
         response = self.get(url, **params)
-        df = pd.json_normalize(response['data'])
+        df = pd.json_normalize(response["data"])
         return df
 
     def get_listings(self, **params: Union[dict, object]) -> pd.DataFrame:
         """See also: https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest"""
         url = self.ENDPOINTS["listings"]
         response = self.get(url, **params)
-        df = pd.json_normalize(response['data'])
+        df = pd.json_normalize(response["data"])
         return df
 
     def get_cryptos_names(self, **params: Union[dict, object]):
         """See also https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyMap"""
         url = self.ENDPOINTS["map"]
         response = self.get(url, **params)
-        df = pd.json_normalize(response['data'])
-        return df[['id', 'name', 'symbol', 'slug', 'rank']]
+        df = pd.json_normalize(response["data"])
+        return df[["id", "name", "symbol", "slug", "rank"]]
 
 
 if __name__ == "__main__":
     cmc = CryptoMarket()
-    parameters = {'slug': 'bitcoin',
-                  'convert': 'EUR'}
+    parameters = {"slug": "bitcoin", "convert": "EUR"}
     # info = cmc.get_quotes(**parameters)
     # pprint.pprint(info)
     #
