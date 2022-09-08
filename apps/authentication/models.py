ec322d9ca48bc44df0fce79d5c87e78ad51fe869
AnnthomyGILLES <annthomy.gilles@gmail.com>
2022-09-08 17:41:12 +0200

    Create Alerts table, linked to an user

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -4,44 +4,53 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
 from apps import db, login_manager
 from apps.authentication.util import hash_pass
 
 
 class Crypto(db.Model):
-    __tablename__ = 'cryptos'
+    __tablename__ = "cryptos"
 
     id = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.String(64), unique=True)
     symbol = db.Column(db.String(64), unique=True)
     # cmc_id = db.Column(db.Integer, unique=True)
-    users_name = db.Column(db.String(64), db.ForeignKey('users.username'))
+    users_name = db.Column(db.String(64), db.ForeignKey("users.username"))
+    crypto_id = db.relationship("Alert", backref="cryptos")
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+
+class Alert(db.Model):
+    __tablename__ = "alerts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    low_threshold = db.Column(db.Integer, nullable=True)
+    high_threshold = db.Column(db.Integer, nullable=True)
+    symbol = db.Column(db.String(64), db.ForeignKey("cryptos.symbol"))
+    user_id = db.Column(db.String(64), db.ForeignKey("users.id"))
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.LargeBinary)
 
-    crypto_id = db.relationship('Crypto', backref='users')
+    crypto_id = db.relationship("Crypto", backref="users")
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
             # depending on whether value is an iterable or not, we must
             # unpack it's value (when **kwargs is request.form, some values
             # will be a 1-element list)
-            if hasattr(value, '__iter__') and not isinstance(value, str):
+            if hasattr(value, "__iter__") and not isinstance(value, str):
                 # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
                 value = value[0]
 
-            if property == 'password':
+            if property == "password":
                 value = hash_pass(value)  # we need bytes here (not plain str)
 
             setattr(self, property, value)
@@ -57,6 +66,6 @@ def user_loader(id):
 
 @login_manager.request_loader
 def request_loader(request):
-    username = request.form.get('username')
+    username = request.form.get("username")
     user = User.query.filter_by(username=username).first()
     return user if user else None
