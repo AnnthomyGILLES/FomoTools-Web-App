ebb6e76d72f6cc06beacc602777547d4aa42d901
AnnthomyGILLES <annthomy.gilles@gmail.com>
2022-09-18 19:19:55 +0200

    Update threshold for handling low and high threshold

--- apps/coinmarketcap/threshold_observer.py
+++ apps/coinmarketcap/threshold_observer.py
@@ -3,30 +3,24 @@ import pandas as pd
 
 from apps.coinmarketcap.coinmarketcap_api import CryptoMarket
 
-if __name__ == "__main__":
+
+def detect_threshold(data):
     cmc = CryptoMarket()
     df = cmc.get_listings(convert="EUR")
     df = df[["symbol", "quote.EUR.price"]].rename(
         columns={"symbol": "Symbol", "quote.EUR.price": "Price"}
     )
 
-    data = [
-        ["BTC", 19870, 19860, "blackperl"],
-        ["ETH", 1574.9, 1575.02, "blackperl"],
-        ["ETH", 1574.6, 1574.7, "gotama"],
-        ["ETH", 1576, 1574.99, "gotama"],
-    ]
     df_obs = pd.DataFrame(
-        data, columns=["Symbol", "ReferencePrice", "Threshold", "User"]
+        data,
+        columns=["Symbol", "ReferencePrice", "LowThreshold", "HighThreshold", "User"],
     )
-
     data = df_obs.merge(df, on="Symbol", how="left")
-
-    data["decreasing"] = (data["Threshold"] <= data["ReferencePrice"]) & (
-        data["Threshold"] >= data["Price"]
+    data["decreasing"] = (data["LowThreshold"] < data["ReferencePrice"]) & (
+        data["LowThreshold"] >= data["Price"]
     )
-    data["increasing"] = (data["Threshold"] >= data["ReferencePrice"]) & (
-        data["Threshold"] <= data["Price"]
+    data["increasing"] = (data["HighThreshold"] > data["ReferencePrice"]) & (
+        data["HighThreshold"] <= data["Price"]
     )
     conditions = [
         (data["decreasing"] == True) & (data["increasing"] == False),
@@ -34,10 +28,27 @@ if __name__ == "__main__":
         (data["decreasing"] == True) & (data["increasing"] == True),
         (data["decreasing"] == False) & (data["increasing"] == False),
     ]
-
-    choices = ["decreasing", "increasing", "error", False]
+    choices = ["decreasing", "increasing", "error", "drop"]
     data["notification"] = np.select(conditions, choices)
     data = data.drop(["decreasing", "increasing"], axis=1)
+
+    df_user_notifications = data[data["notification"] != "drop"]
+
+    return df_user_notifications
+
+
+def send_notification(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = [
+        ["BTC", 19870, 19860, 19875, "blackperl"],
+        ["ETH", 1700, 1600, 1775, "blackperl"],
+        ["ETH", 1574.6, np.nan, 1574.7, "gotama"],
+        ["ETH", 1700, 1602, np.nan, "gotama"],
+    ]
+    df_user_notifications = detect_threshold(data)
 
 #
 #     'apprise -vv -t "Test Message Title" -b "Test Message Body" \
