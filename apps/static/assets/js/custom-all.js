8757cd8f055152d26a0f13e3eb3039477c1a7f14
Durimel Kevin <k.durimel@gmail.com>
2022-09-19 16:48:13 +0200

    Choices-js used for notification method selection

--- apps/static/assets/js/custom-all.js
+++ apps/static/assets/js/custom-all.js
@@ -1,3 +1,4 @@
+window.onload = function() {
 // get a new date (locale machine date time)
 const date = new Date();
 // get the date as a string
@@ -6,3 +7,25 @@ const datestring = date.toDateString();
 const timestring = date.toLocaleTimeString();
 // Insert time in html
 //document.getElementById('timestring').innerHTML = timestring
+// Setup Choice-js for #price_notification_method 
+const percentNotificationMethodForm = $('#percent_notification_method')[0];
+const percentNotificationMethodChoice = new Choices(percentNotificationMethodForm, {
+    renderChoiceLimit: -1,
+    maxItemCount: 2,
+    removeItems: true,
+    removeItemButton: true,
+    loadingText: 'Chargement...',
+    noResultsText: 'Aucun résultat trouvé',
+    noChoicesText: 'Aucun choix possible à partir de',
+    itemSelectText: 'Cliquez pour sélectionner',
+    addItemText: (value) => {
+      return `Appuyez sur entrée pour ajouter <b>"${value}"</b>`;
+    },
+    maxItemText: (maxItemCount) => {
+      return `Vous pouvez choisir ${maxItemCount} options maximum`;
+    },
+    valueComparer: (value1, value2) => {
+      return value1 === value2;
+    }
+  });
+}
