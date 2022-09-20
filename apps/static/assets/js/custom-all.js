c8fc51d8ff072072b34da55fa708e0c596d5c817
Durimel Kevin <k.durimel@gmail.com>
2022-09-20 22:23:47 +0200

    Choices-js activated and ok for all pricing forms

--- apps/static/assets/js/custom-all.js
+++ apps/static/assets/js/custom-all.js
@@ -1,4 +1,3 @@
-window.onload = function() {
 // get a new date (locale machine date time)
 const date = new Date();
 // get the date as a string
@@ -7,7 +6,8 @@ const datestring = date.toDateString();
 const timestring = date.toLocaleTimeString();
 // Insert time in html
 //document.getElementById('timestring').innerHTML = timestring
-// Setup Choice-js for #price_notification_method 
+// Setup Choice-js for #percent_notification_method 
+window.onload = function() {
 let percentNotificationMethodForm = $('#percent_notification_method')[0];
 let percentNotificationMethodChoice = new Choices(percentNotificationMethodForm, {
     renderChoiceLimit: -1,
@@ -31,6 +31,48 @@ let percentNotificationMethodChoice = new Choices(percentNotificationMethodForm,
 // Setup Choice-js for #percent_crypto_selected 
 let percentCryptoSelectedForm = $('#percent_crypto_selected')[0];
 let percentCryptoSelectedChoice = new Choices(percentCryptoSelectedForm, {
+    renderChoiceLimit: 5,
+    maxItemCount: 1,
+    removeItems: true,
+    removeItemButton: true,
+    loadingText: 'Chargement...',
+    noResultsText: 'Aucun résultat trouvé',
+    noChoicesText: 'Aucun choix possible à partir de',
+    itemSelectText: 'Cliquez pour sélectionner',
+    addItemText: (value) => {
+      return `Appuyez sur entrée pour ajouter <b>"${value}"</b>`;
+    },
+    maxItemText: (maxItemCount) => {
+      return `Vous pouvez choisir ${maxItemCount} options maximum`;
+    },
+    valueComparer: (value1, value2) => {
+      return value1 === value2;
+    }
+  });
+// Setup Choice-js for #price_notification_method 
+let priceNotificationMethodForm = $('#price_notification_method')[0];
+let priceNotificationMethodChoice = new Choices(priceNotificationMethodForm, {
+    renderChoiceLimit: -1,
+    maxItemCount: 2,
+    removeItems: true,
+    removeItemButton: true,
+    loadingText: 'Chargement...',
+    noResultsText: 'Aucun résultat trouvé',
+    noChoicesText: 'Aucun choix possible à partir de',
+    itemSelectText: 'Cliquez pour sélectionner',
+    addItemText: (value) => {
+      return `Appuyez sur entrée pour ajouter <b>"${value}"</b>`;
+    },
+    maxItemText: (maxItemCount) => {
+      return `Vous pouvez choisir ${maxItemCount} options maximum`;
+    },
+    valueComparer: (value1, value2) => {
+      return value1 === value2;
+    }
+  });
+// Setup Choice-js for #price_crypto_selected 
+let priceCryptoSelectedForm = $('#price_crypto_selected')[0];
+let priceCryptoSelectedChoice = new Choices(priceCryptoSelectedForm, {
     renderChoiceLimit: 5,
     maxItemCount: 1,
     removeItems: true,
